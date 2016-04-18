7f65481e54805bd0b95056843c2cd459e4e0f80f
ankitbajaj <ankitbajaj143@gmail.com>
2016-04-18 01:01:16 +0100

    error

--- app/src/main/java/com/example/admin/firstproject/MainActivity.java
+++ app/src/main/java/com/example/admin/firstproject/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_main);
         RelativeLayout mainLayout = new RelativeLayout(this);
 
-        Button button = new Button(this);
+        Button button = new Butto(this);
         button.setText("Click Me change by Muthu for pull request");
 
 
